f6974fcf7dafad6789a40f8161d1f6f2fccd64d0
danimatuko <danimatuko@outlook.com>
2024-12-10 19:14:10 +0200

    add about screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
+      <Link href="/about" style={styles.button}>Go to about screen</Link>
     </View>
   );
 }
@@ -18,5 +20,10 @@ const styles = StyleSheet.create({
   },
   text: {
     color: "#fff"
-  }
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
 })
