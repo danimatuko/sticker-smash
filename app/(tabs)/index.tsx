caa803ebc866e554a7fbff7d03d5de9886b7f882
danimatuko <danimatuko@outlook.com>
2024-12-11 00:28:21 +0200

    Create Button componnet and add buttons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { View, StyleSheet } from 'react-native';
 import ImageViewer from '@/components/ImageViewer';
+import Button from '@/components/Button';
 
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
@@ -10,6 +11,10 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme='primary' label='Choose a photo' />
+        <Button label='Use a photo' />
       </View>
     </View>
   );
@@ -23,11 +28,11 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 1,
+    paddingTop: 28,
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
 
